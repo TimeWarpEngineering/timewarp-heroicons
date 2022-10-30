470cf2357c9b2b1f417046229da9d309a147ba10
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-10-30 04:07:49 +0000

    Update scripts to use variables

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,8 +1,10 @@
 Push-Location $PSScriptRoot
 try {
     if (!$Nuget_Key) { throw "Nuget_Key is not set"}
-    dotnet pack ./Source/timewarp-heroicons/timewarp-heroicons.csproj -c Release
+    dotnet pack ../Source/timewarp-heroicons/timewarp-heroicons.csproj -c Release
+    Push-Location ../Source/timewarp-heroicons/bin/Release
     dotnet nuget push **/*.nupkg --source https://api.nuget.org/v3/index.json --api-key $Nuget_Key    
+    Pop-Location
 }
 finally {
     Pop-Location
