b8a965129023511a11edae8b5db053e2ccfdaa6e
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-10-30 05:00:08 +0000

    use update version

--- releases.md
+++ releases.md
@@ -1,9 +1,10 @@
 # Releases
 
-## 2.0.12+build-2
+## 2.0.12-update.1
 
 * Changed from ComponentBase to IComponent ( Thanks @chan18 )
 * Updated PowerShell scripts
+* Released as update since we already used the version number and Heroicons has not updated
 
 ## 2.0.12+build-1
 
--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,8 +1,10 @@
-Push-Location $PSScriptRoot
+Push-Location "$PSScriptRoot/.."
 try {
     if (!$Nuget_Key) { throw "Nuget_Key is not set"}
-    dotnet pack ../Source/timewarp-heroicons/timewarp-heroicons.csproj -c Release
-    Push-Location ../Source/timewarp-heroicons/bin/Release
+    dotnet tool restore
+    dotnet cleanup -y
+    dotnet pack ./Source/timewarp-heroicons/timewarp-heroicons.csproj -c Release
+    Push-Location ./Source/timewarp-heroicons/bin/Release
     dotnet nuget push **/*.nupkg --source https://api.nuget.org/v3/index.json --api-key $Nuget_Key    
     Pop-Location
 }
